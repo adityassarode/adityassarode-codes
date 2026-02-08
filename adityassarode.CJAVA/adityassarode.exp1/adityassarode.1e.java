e42c5ce21adcf6a6583efd6843e9ee62a905f39e
Aditya Sarode <adityassarode@outlook.in>
2026-02-08 13:06:18 +0530

    Update adityassarode.1e.java

--- adityassarode.CJAVA/adityassarode.exp1/adityassarode.1e.java
+++ adityassarode.CJAVA/adityassarode.exp1/adityassarode.1e.java
@@ -3,6 +3,11 @@
 //1e. Implement a program to print a simple pattern, such as a triangle or a square.
 
 
+//Aditya Sarode
+
+//1e. Implement a program to print a simple pattern, such as a triangle or a square.
+
+
 import java.util.Scanner;
 
 class Pattern {
@@ -12,11 +17,23 @@ class Pattern {
         System.out.println("Enter number of rows:");
         int n = sc.nextInt();
 
+        System.out.println("Tirangle: \n");
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nSquare: \n");
+
         for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= i; j++) {
+            for(int j = 1; j <= n; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
 }
+
